f0486291533209a208d6ea4eddeee4850e644bc4
Lucas Bernardes <lucasbernardes24@gmail.com>
2022-07-19 16:07:08 -0300

    test profile & H2 db

--- src/main/java/com/lbernardes03/project/entities/User.java
+++ src/main/java/com/lbernardes03/project/entities/User.java
@@ -2,7 +2,15 @@ package com.lbernardes03.project.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "users")
 public class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
